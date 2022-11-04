1338f3964aa53677a07d7153210c669a4ba0b0a2
Mason Wudtee <mason.wudtee@gmail.com>
2022-11-04 16:27:45 -0500

    albums_migration working

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,12 +3,12 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
-   id INT NOT NULL AUTO_INCREMENT,
+   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
    artist VARCHAR(25) NOT NULL,
-   name VARCHAR(25) NOT NULL ,
-   release_date INT(4),
-   sales FLOAT(7),
-   genre VARCHAR(25),
+   title VARCHAR(25) NOT NULL,
+   release_date INT UNSIGNED NOT NULL,
+   sales FLOAT NOT NULL,
+   genre VARCHAR(25) NOT NULL,
 
    PRIMARY KEY (id)
 );
