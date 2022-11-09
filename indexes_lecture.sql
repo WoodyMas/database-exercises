665bc951daf0073baca59d2f56f2be6ac2c1ebd9
Mason Wudtee <mason.wudtee@gmail.com>
2022-11-09 16:00:49 -0600

    indexes lectures complete

---
+++ indexes_lecture.sql
@@ -0,0 +1,15 @@
+USE employees;
+
+SELECT * FROM employees e;
+
+SHOW INDEXES FROM employees;
+
+SHOW INDEXES FROM salaries;
+
+SELECT salary FROM salaries WHERE salary BETWEEN 50000 AND 70000;
+
+ALTER TABLE salaries ADD INDEX salary_index (salary);
+
+ALTER TABLE salaries DROP INDEX salary_index;
+
+# ALTER TABLE tbl_name ADD UNIQUE (column_name...)
